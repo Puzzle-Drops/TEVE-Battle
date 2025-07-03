8ddca8c38cf75ffad9e2cb90d530922f3b40eae1
Puzzle-Drops <86485071+Puzzle-Drops@users.noreply.github.com>
2025-07-03 13:44:48 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1254,7 +1254,7 @@ class Game {
         
         // Close when clicking elsewhere
         setTimeout(() => {
-            document.addEventListener('click', this.uiManager.closeItemContextMenu(), { once: true });
+            document.addEventListener('click', this.uiManager.closeItemContextMenu().bind(this.uiManager), { once: true });
         }, 10);
     }
 
