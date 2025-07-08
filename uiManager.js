3d67e83e7b6877a091095f05ba8ee67c55f43dcb
Puzzle-Drops <86485071+Puzzle-Drops@users.noreply.github.com>
2025-07-08 13:09:32 -0400

    Update uiManager.js

--- uiManager.js
+++ uiManager.js
@@ -383,8 +383,8 @@ showHeroes() {
     
     // Check if we need to create more heroes
     if (this.game.maxPartySize > this.game.heroes.length) {
-        this.tutorial.npcDialogue('Skypper', ["Hello there, adventurer! I've got a new recruit for you! Don't be shy, introduce yourself."], false);
-        this.tutorial.showNewHeroCreation();
+        this.game.tutorial.npcDialogue('Skypper', ["Hello there, adventurer! I've got a new recruit for you! Don't be shy, introduce yourself."], false);
+        this.game.tutorial.showNewHeroCreation();
     }
     
 }
